6179c9a289d4de78e5ef44042d27c1b74d118ddd
lucasdasilvabezerra <127355535+lucasdasilvabezerra@users.noreply.github.com>
2024-02-17 20:48:12 -0300

    Concluído

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
-console.log("Teste")
+
+
+const buttons = document.querySelectorAll("#image-picker li");
+const image = document.querySelector("#product-image")
+let imgSrc = image.getAttribute("src")
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        console.log(e);
+
+        buttons.forEach((btn) => {
+            btn.querySelector(".color").classList.remove("selected")
+        });
+
+        const button = e.target
+
+        const id = button.getAttribute("id")
+
+        button.querySelector(".color").classList.add("selected")
+
+        
+        image.classList.add("changing")
+        image.setAttribute("src",`img/iphone_${id}.jpg`)
+        
+        setTimeout(()=>{
+            image.classList.toggle("changing")
+        },200)
+
+    });
+
+
+})
